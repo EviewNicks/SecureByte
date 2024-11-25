b57db28e3cdfdc55bd6b8f1d94b272eab087d255
Ardiansyah <eviewnicks@gmail.com>
2024-11-25 19:56:35 +0800

    update config

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,14 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/var/www/",
   plugins: [react()],
   css: {
     postcss: "./postcss.config.js",
   },
   server: {
+    host: "0.0.0.0",
+    port: 3000,
     historyApiFallback: true,
   },
 });
